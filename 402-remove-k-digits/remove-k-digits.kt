a97d6fc0ed05fcaf0a7efb120ba27369406cda4d
Mark <mark-brannan@users.noreply.github.com>
2024-04-11 21:23:26 -0700

    Time: 296 ms (48.48%) | Memory: 41.1 MB (33.33%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun removeKdigits(num: String, k: Int): String {
+        var k = k
+        val stack = ArrayDeque<Char>()
+        num.forEach {
+            while (k > 0 && stack.isNotEmpty() && stack.last() > it) {
+                --k
+                stack.removeLast()
+            }
+            stack.add(it)
+        }
+        while (stack.isNotEmpty() && k > 0) {
+            stack.removeLast()
+            --k
+        }
+
+        val result = stack.joinToString("").trimStart('0')
+        return if (result.isNotEmpty()) result else "0"
+        
+    }
+}
