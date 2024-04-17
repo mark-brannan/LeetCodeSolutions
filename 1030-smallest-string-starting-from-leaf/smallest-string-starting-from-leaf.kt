2f81af6afe6174a770ee80536b4063414b936334
Mark <mark-brannan@users.noreply.github.com>
2024-04-17 12:32:16 -0700

    Time: 232 ms (25.00%) | Memory: 40.4 MB (25.00%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.kt
@@ -0,0 +1,41 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun smallestFromLeaf(root: TreeNode?): String {
+        val currentString = ""
+        if (root == null) return currentString
+
+        var smallest = Char.MAX_VALUE + ""
+        val stack = ArrayDeque<Pair<TreeNode, String>>()
+
+        stack.push(root to "")
+        while (stack.isNotEmpty()) {
+            val (next, priorString) = stack.pop()
+            val newString = next.toChar() + priorString
+
+            if (next.isLeaf()) {
+                //System.out.println("Leaf: $newString, smallest=$smallest")
+                smallest = minOf(smallest, newString)
+            }
+
+            next.left?.let { stack.push(it to newString) }
+            next.right?.let { stack.push(it to newString) }
+        }
+        return smallest
+    }
+
+    // ASCII 97 is 'a'
+    internal fun TreeNode.toChar() = (this.`val` + 97).toChar()
+}
+
+fun TreeNode.isLeaf() = (left == null && right == null)
+fun <T> ArrayDeque<T>.push(it: T) = this.add(it)
+fun <T> ArrayDeque<T>.pop() = this.removeLast()
