71137f1784a9393b611bafe2d1c9650cbec376c5
Mark <mark-brannan@users.noreply.github.com>
2024-05-10 18:25:27 -0700

    Time: 319 ms (97.90%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -4,9 +4,7 @@ with emp_with_primary as (
     where primary_flag = 'Y'
 ),
 emp_with_dept_count as (
-    select
-        employee_id, department_id,
-        count(department_id) over (partition by employee_id) as dept_count
+    select employee_id, department_id, count(department_id) over (partition by employee_id) as dept_count
     from Employee
 ),
 primary_union as (
