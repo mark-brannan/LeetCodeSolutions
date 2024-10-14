318b04678b83b106a5b9ea65d4b3ae363d246828
Mark <mark-brannan@users.noreply.github.com>
2024-10-14 11:58:57 -0700

    Time: 242 ms (47.29%) | Memory: 40.3 MB (42.64%) - LeetSync

--- 135-candy/candy.kt
+++ 135-candy/candy.kt
@@ -2,16 +2,16 @@ class Solution {
     fun candy(ratings: IntArray): Int {
         val candies = IntArray(ratings.size) { 1 }
 
-        for (i in 0..ratings.lastIndex) {
+        for (i in 1..ratings.lastIndex) {
             // has a higher rating than child on left, but less candy
-            if (i > 0 && ratings[i] > ratings[i-1] && candies[i] <= candies[i-1]) {
+            if (ratings[i] > ratings[i-1] && candies[i] <= candies[i-1]) {
                 candies[i] = candies[i-1] + 1
             }
         }
 
-        for (i in ratings.lastIndex downTo 0) {
+        for (i in ratings.lastIndex - 1 downTo 0) {
             // has a higher rating than child on right, but less candy
-            if (i < ratings.lastIndex && ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) {
+            if (ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) {
                 candies[i] = candies[i+1] + 1
             }
         }
