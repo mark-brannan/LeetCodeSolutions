498c84d020a40eb8f38f657c39ff830a83ec5677
Mark <mark-brannan@users.noreply.github.com>
2024-12-10 16:04:11 -0800

    Time: 257 ms (51.28%) | Memory: 39 MB (43.14%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.kt
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun countConsistentStrings(allowed: String, words: Array<String>): Int {
-        val allowedSet = allowed.map { it }.toSet()
+        val allowedSet = allowed.map { it }.toHashSet()
         fun String.isAllowed() = this.all { allowedSet.contains(it) }
 
         return words.count { it.isAllowed() }
