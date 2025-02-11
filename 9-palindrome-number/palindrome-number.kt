434579b76f1d24b757bd1c33482276750d6574a8
Mark <mark-brannan@users.noreply.github.com>
2025-02-11 12:45:42 -0800

    Time: 25 ms (6.57%) | Memory: 38.5 MB (30.43%) - LeetSync

--- 9-palindrome-number/palindrome-number.kt
+++ 9-palindrome-number/palindrome-number.kt
@@ -3,7 +3,7 @@ class Solution {
         return when {
             x < 0 -> false
             x < 10 -> true // optimization for single digit ints
-            x > 1000000000 && x.toString().reversed().toDouble() > Int.MAX_VALUE -> return false
+            x > 1000000000 && x.toString().reversed().toDouble() > Int.MAX_VALUE -> false
             else -> {
                 val xPrime = x.toString().reversed().toInt()
                 x == xPrime
