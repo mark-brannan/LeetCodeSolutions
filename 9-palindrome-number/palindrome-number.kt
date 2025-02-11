f3e989e153403f965478e43f00f1796cbcdf260c
Mark <mark-brannan@users.noreply.github.com>
2025-02-11 12:52:57 -0800

    Time: 19 ms (25.75%) | Memory: 39.1 MB (11.49%) - LeetSync

--- 9-palindrome-number/palindrome-number.kt
+++ 9-palindrome-number/palindrome-number.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
         return when {
-            x == 0 -> true
             x < 0 -> false
+            x < 10 -> true
             x % 10 == 0 -> false
             else -> x.toString().let { it == it.reversed() }
         }
