23aa11a86859c81c93103931953091dc18656c4a
Mark <mark-brannan@users.noreply.github.com>
2025-02-11 12:45:27 -0800

    Time: 29 ms (5.57%) | Memory: 38.8 MB (21.79%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        return when {
+            x < 0 -> false
+            x < 10 -> true // optimization for single digit ints
+            x > 1000000000 && x.toString().reversed().toDouble() > Int.MAX_VALUE -> return false
+            else -> {
+                val xPrime = x.toString().reversed().toInt()
+                x == xPrime
+            }
+        }
+    }
+}
