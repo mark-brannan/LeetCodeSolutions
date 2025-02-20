7142a3b4f01a6529bb6f3cb3af995abe4ef9a33c
Mark <mark-brannan@users.noreply.github.com>
2025-02-20 11:11:11 -0800

    Time: 73 ms (0.00%) | Memory: 45.3 MB (16.67%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.kt
+++ 2107-find-unique-binary-string/find-unique-binary-string.kt
@@ -1,11 +1,13 @@
+fun String.toIntFromBinary() = this.toInt(2)
+fun Int.toBinaryString() = Integer.toBinaryString(Integer.valueOf(this))
+fun Int.toBinaryString(n: Int) = this.toBinaryString().padStart(n, '0')
 
 class Solution {
     fun findDifferentBinaryString(nums: Array<String>): String {
         val n = nums[0].length
-        val max = "1".repeat(n).toInt(2)
-        val set = nums.map { it.toInt(2) }.sorted().toSet()
-        val differentInt = (0..max).toSet().subtract(set).first()
-        val javaInteger = Integer.valueOf(differentInt)
-        return Integer.toBinaryString(javaInteger).padStart(n, '0')
+        val max = "1".repeat(n).toIntFromBinary()
+        val set = nums.map { it.toIntFromBinary() }.toHashSet()
+        val differentInt = (0..max).filter { it !in set }.first()
+        return differentInt.toBinaryString(n)
     }
 }
