79f87711dc86551e43df72edf2c0ea79202858ef
Mark <mark-brannan@users.noreply.github.com>
2025-06-27 16:50:55 -0700

    Time: 1 ms (21.69%) | Memory: 43 MB (40.32%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.kt
@@ -0,0 +1,51 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        val dummy = ListNode(-1)
+        var headPair = Pair(list1, list2)
+        var current = dummy
+
+        while (current != null) {
+            with (headPair.orderNodes()) {
+                current.next = first
+                headPair = Pair(first?.next, second)
+                current = current.next
+            }
+        }
+        return dummy.next
+    }
+}
+
+fun Pair<ListNode?, ListNode?>.orderNodes(): Pair<ListNode?, ListNode?> {
+    return when {
+     first == null -> second to first
+     second == null -> first to second
+     first!!.`val` < second!!.`val` -> first to second
+     else -> second to first
+    }
+}
+
+fun Pair<ListNode?, ListNode?>.niceString() = "(${first?.`val`}, ${second?.`val`})"
+
+fun ListNode?.niceString(): String {
+    val builder = StringBuilder()
+    builder.append("[")
+    var next = this
+    while (next != null) {
+        builder.append(next.`val`)
+        next = next.next
+        if (next != null) {
+            builder.append(",")
+        }
+    }
+    builder.append("]")
+    return builder.toString()
+}
