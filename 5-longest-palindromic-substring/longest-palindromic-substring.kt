02c849c7ea3c8ebc15bcb19132430ceebcbbd46d
Mark <mark-brannan@users.noreply.github.com>
2025-06-27 20:30:22 -0700

    Time: 20 ms (68.63%) | Memory: 46.1 MB (43.58%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.kt
+++ 5-longest-palindromic-substring/longest-palindromic-substring.kt
@@ -1,12 +1,11 @@
 class Solution {
     fun longestPalindrome(s: String): String {
-        var longest = ""
-        var length = 0
+        var longest = s.substring(0,1)
+        var length = longest.length
 
         fun check(leftStart: Int, rightStart: Int) {
             var (l, r) = leftStart to rightStart
             while (l >= 0 && r < s.length && s[l] == s[r]) {
-                val substring = s.substring(l, r + 1)
                 if (r - l + 1 > length) {
                     length = r - l + 1
                     longest = s.substring(l, r + 1)
