9153f9339571b0372a110a44f1ee3035e256810e
Mark <mark-brannan@users.noreply.github.com>
2025-06-27 20:23:34 -0700

    Time: 63 ms (42.53%) | Memory: 46.5 MB (40.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.kt
+++ 5-longest-palindromic-substring/longest-palindromic-substring.kt
@@ -3,8 +3,8 @@ class Solution {
         var longest = ""
         var length = 0
 
-        fun check(left: Int, right: Int) {
-            var (l, r) = left to right
+        fun check(leftStart: Int, rightStart: Int) {
+            var (l, r) = leftStart to rightStart
             while (l >= 0 && r < s.length && s[l] == s[r]) {
                 val substring = s.substring(l, r + 1)
                 if (r - l + 1 > length) {
@@ -17,24 +17,9 @@ class Solution {
         }
 
         for (i in s.indices) {
-            check(i, i)
-            check(i, i + 1)
+            check(i, i) // odd lengths
+            check(i, i + 1) // even lengths
         }
         return longest
-    }
-
-    fun String.isPalindrome(): Boolean {
-        for (i in 0..lastIndex) {
-            val j = lastIndex - i
-            when {
-                this[i] == this[j] && i < j -> continue
-                this[i] == this[j] && i >= j -> {
-                    // System.out.println("palindrome: $this, $i, $j, ${this[i]} vs ${this[j]}")
-                    return true
-                }
-                else -> break
-            }
-        }
-        return false
     }
 }
