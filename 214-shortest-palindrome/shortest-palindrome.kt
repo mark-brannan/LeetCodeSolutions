3964f71089754edc10e88692f9631580c147650b
Mark <mark-brannan@users.noreply.github.com>
2025-06-27 22:22:55 -0700

    Time: 1443 ms (12.50%) | Memory: 48.3 MB (37.50%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.kt
+++ 214-shortest-palindrome/shortest-palindrome.kt
@@ -24,60 +24,15 @@ class Solution {
 }
 
 fun isPalindrome(i: Int, prefix: CharArray, s: String): Boolean {
-    // System.out.println("Checking isPalindrome(): ${prefix.concatToString(0, maxOf(0, i))} + $s, ${i}, ${s.lastIndex}")
     var (left, right) = 0 to s.lastIndex
-    while (left < (right + (i + 1))) {
+    while (left < right + i + 1) {
         val leftChar = when {
-            left > i -> {
-                val leftShifted = left - (i + 1) // 0 when left=0, i=-1, 0 when left=1, i=0, 1 when left=1, i=-1
-                //System.out.println("$left > $i -> ${s[leftShifted]}")
-                s[leftShifted]
-            }
-            else -> {
-                //System.out.println("prefix[$left]=${prefix[left]}")
-                prefix[left]
-            }
+            left > i -> s[left - (i + 1)]
+            else -> prefix[left]
         }
-        val rightChar = s[right]
-        //System.out.println("$left, $right: $leftChar vs $rightChar (${i})")
-        if (leftChar != rightChar) return false
+        if (leftChar != s[right]) return false
         ++left 
         --right
     }
     return true
-        //.also { System.out.println("isPalindrome($catted) is $it") }
-}
-
-
-/*fun isPalindrome(prefix: CharArray, s: String): Boolean {
-    System.out.println("Checking isPalindrome(): ${prefix.concatToString()} + $s")
-    var (i, j) = 0 to s.length
-    var (left, right) = 0 to (prefix.lastIndex + s.lastIndex)
-    while (left < right) {
-        val leftChar = when {
-            left <= prefix.lastIndex -> prefix[left]
-            else -> s[left - prefix.lastIndex]
-        }
-        val rightChar = s[right - prefix.lastIndex]
-        System.out.println("$left, $right: $leftChar vs $rightChar...${left - prefix.lastIndex}, ${right - prefix.lastIndex}")
-        if (leftChar != rightChar) return false
-        ++left 
-        --right
-    }
-    return true
-        //.also { System.out.println("isPalindrome($catted) is $it") }
-}*/
-
-fun String.isPalindrome(): Boolean {
-    if (length <= 1) return true
-
-    for (i in 0..lastIndex) {
-        val j = lastIndex - i
-        when {
-            this[i] == this[j] && i < j -> continue
-            this[i] == this[j] && i >= j -> return true
-            else -> break
-        }
-    }
-    return false
 }
