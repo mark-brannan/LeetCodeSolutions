ef509cfc0c584ae5f470fe790f53d32940d33877
Mark <mark-brannan@users.noreply.github.com>
2025-07-01 12:06:04 -0700

    Time: 74 ms (14.45%) | Memory: 73.5 MB (12.14%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.kt
@@ -0,0 +1,24 @@
+
+class KthLargest(k: Int, nums: IntArray) {
+    val minQueue = PriorityQueue<Int>(nums.sorted().takeLast(k))
+    init {
+        while (minQueue.size < k) {
+            minQueue.add(Int.MIN_VALUE)
+        }
+    }
+
+    fun add(`val`: Int): Int {
+        if (`val` > minQueue.peek() ?: 0) {
+            minQueue.poll()
+            minQueue.add(`val`)
+        }
+        return minQueue.peek()
+    }
+
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = KthLargest(k, nums)
+ * var param_1 = obj.add(`val`)
+ */
