f3f16c8528395d712004a828d63db0528630262d
Mark <mark-brannan@users.noreply.github.com>
2025-07-01 16:18:05 -0700

    Time: 13 ms (21.20%) | Memory: 43.5 MB (37.97%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.kt
+++ 14-longest-common-prefix/longest-common-prefix.kt
@@ -1,8 +1,13 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
+        // LCP cannot longer than the shortest input string
         val maxPrefixLength = strs.map { it.length }.min()
         val prefixBuilder = StringBuilder(maxPrefixLength)
 
+        // Consider the set of distinct characters for each string at position i:
+        // If all the stings have the same char at that point, then it's part of the LCP.
+        // If we have more than one character at that point, then it is not part of LCP
+        // and we can halt.
         outerLoop@ for (i in 0..(maxPrefixLength-1)) {
             val nextChars = strs.map { it[i] }.distinct()
             when (nextChars.size) {
